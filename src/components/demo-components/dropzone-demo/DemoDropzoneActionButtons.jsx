94debcd615facbdf42ad7b77394e84ee1ed139b8
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2023-03-17 00:50:16 -0500

    [WIP][FEAT]: Add action buttons demo section for FileInputButton

--- src/components/demo-components/dropzone-demo/DemoDropzoneActionButtons.jsx
+++ src/components/demo-components/dropzone-demo/DemoDropzoneActionButtons.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { Dropzone, FileMosaic } from "../../../files-ui";
+import { Dropzone, FileCard, FileInputButton, FileMosaic } from "../../../files-ui";
 
-const DemoDropzoneActionButtons = ({button}) => {
+const DemoDropzoneActionButtons = ({ button }) => {
   const [files, setFiles] = React.useState([]);
   const updateFiles = (incommingFiles) => {
     //do something with the files
@@ -11,6 +11,56 @@ const DemoDropzoneActionButtons = ({button}) => {
   const removeFile = (id) => {
     setFiles(files.filter((x) => x.id !== id));
   };
+
+  if (button)
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          gap: "10px",
+          flexWrap: "wrap",
+          width: "100%",
+        }}
+      >
+        <FileInputButton
+          onChange={updateFiles}
+          value={files}
+          autoClean
+          uploadConfig={{ url: "https://www.myawsomeserver.com/upload" }}
+          fakeUpload
+          actionButtons={{
+            position: "bottom",
+            uploadButton: { style: { textTransform: "uppercase" } },
+            abortButton: {},
+            cleanButton: {},
+            deleteButton: {},
+          }}
+        />
+        {files.length > 0 && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexWrap: "wrap",
+              gap: "5px",
+              minWidth: "50%",
+            }}
+          >
+            {files.map((file) => (
+              <FileCard
+                key={file.id}
+                {...file}
+                onDelete={removeFile}
+                info
+                preview
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    );
   return (
     <Dropzone
       onChange={updateFiles}
