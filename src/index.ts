ec559485f25e85592b321acedc1c2de657e7b18c
Florian Spreckelsen <f.spreckelsen@indiscale.com>
2024-08-09 15:20:16 +0200

    [REFACTOR]: Use fontsource/poppins instead of googlefonts

--- src/index.ts
+++ src/index.ts
@@ -155,3 +155,11 @@ export * from "./FilesUiProvider/FilesUiProvider";
 
 
 export type { IconsSet, FilesUIConfig } from "./FilesUiProvider";
+
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
